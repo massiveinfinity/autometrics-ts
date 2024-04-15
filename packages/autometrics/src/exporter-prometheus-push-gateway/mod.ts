1e182732baec9bb50caa8a560c90a21f7d3cbe85
barry <barry@massiveinfinity.com>
2024-04-15 16:55:39 +0800

    Fix to standardize environment variable name for tenant id

--- packages/autometrics/src/exporter-prometheus-push-gateway/mod.ts
+++ packages/autometrics/src/exporter-prometheus-push-gateway/mod.ts
@@ -75,7 +75,7 @@ export function init({
     return;
   }
 
-  if (!tenantId && !process.env.MASSIVE_DSO_TENANT_ID) {
+  if (!tenantId && !process.env.MASSIVE_TENANT_ID) {
     amLogger.warn('No values defined for `tenantId`. This is a required parameter so that we can identify you.');
     return;
   }
